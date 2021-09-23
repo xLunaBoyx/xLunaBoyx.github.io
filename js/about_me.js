04a3c53f95cea81f3d0bc87a08a8e03ea5a1f96e
xLunaBoyx <sonic_x@naver.com>
2021-09-23 23:51:08 +0900

    Initial commit

---
+++ js/about_me.js
@@ -0,0 +1,26 @@
+// 맨 위로 가기 버튼
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// 딱히 뭔가 하지 않아도 자동으로 실행되도록 한 함수들
+$(document).ready(function() {
+    $(".container").delay(200).animate({opacity: 1}, 800);
+});
